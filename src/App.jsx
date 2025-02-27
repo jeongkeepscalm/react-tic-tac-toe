5bedda074a4c2949f41bcc3322f376d6547ed09b
ojg <jeongkeepscalm@gmail.com>
2025-02-27 18:06:02 +0900

    done

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,20 @@ import Log from "./components/Log.jsx";
 import { WINNING_COMBINATIONS } from "./winning-combinations.js";
 import GameOver from "./components/GameOver.jsx";
 
-// 초기 게임 보드 상태
+// 초기 게임 보드
 const INITIAL_GAME_BOARD = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
+
+// 초기 플레이어 
 const PLAYERS = {
   X : '사용자1',
   O : '사용자2'
 };
 
-// 현재 플레이어의 상태를 보여준다. 
+// [Helper Function] 현재 플레이어의 상태를 보여준다. 
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = 'X';
   if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
@@ -25,24 +27,20 @@ function deriveActivePlayer(gameTurns) {
   return currentPlayer;
 }
 
-
+// [Helper Function] 게임 턴을 기반으로 게임 판을 업데이트한다.
 function deriveGameBoard(gameTurns) {
-    // 초기 게임 보드판 깊은 복사
-    let gameBoard = [...INITIAL_GAME_BOARD.map((row) => [...row])];
-  
-    // 게임 턴을 기반으로 게임 판을 업데이트한다.
+    let gameBoard = [...INITIAL_GAME_BOARD.map((row) => [...row])]; // 깊은 복사
     for (const turn of gameTurns) {
       const {square, player} = turn;
       const {row, cell} = square;
       gameBoard[row][cell] = player;
     }
-
     return gameBoard;
 }
 
+// [Helper Function] 게임 보드를 기반으로 승자를 결정한다.
 function deriveWinner(gameBoard, players) {
   let winner;
-  // 게임 판을 비교하여 승자를 결정한다.
   for (const combination of WINNING_COMBINATIONS) {
     const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
     const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
@@ -54,20 +52,20 @@ function deriveWinner(gameBoard, players) {
       winner = players[firstSquareSymbol];
     }
 
-    // if (gameBoard.flat().every((cell) => cell !== null) && !winner) {
-    //   winner = '무승부';
-    // }
+    /*
+      if (gameBoard.flat().every((cell) => cell !== null) && !winner) {
+        winner = '무승부';
+      }
+    */
   }
   return winner;
 }
-
 
 
 function App() {
 
   const [players, setPlayers] = useState(PLAYERS);
   const [gameTurns, setGameTurns] = useState([]);
-
   const activePlayer = deriveActivePlayer(gameTurns);
   const gameBoard = deriveGameBoard(gameTurns);
   const winner = deriveWinner(gameBoard, players);
@@ -82,8 +80,7 @@ function App() {
         { square: {row: rowIndex, cell: cellIndex}, player: activePlayer }
         , ...prevGameTurns
       ];
-      console.log(updatedTurns)
-        return updatedTurns;
+      return updatedTurns;
     });
   };
 
@@ -95,7 +92,6 @@ function App() {
 
   // 유저 이름 변경 이벤트 핸들러
   function handlePlayerNameChange(symbol, newName) {
-    console.log(symbol, newName)
     setPlayers((prevPlayers) => {
       return {
         ...prevPlayers,
